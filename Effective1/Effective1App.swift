4f5f784f684e27f1a8cd3ffebc49a6041bfa3637
ewrika <egorkaomsk_2003@mail.ru>
2023-11-13 15:35:48 +0600

    Initial Commit

---
+++ Effective1/Effective1App.swift
@@ -0,0 +1,17 @@
+//
+//  Effective1App.swift
+//  Effective1
+//
+//  Created by Георгий Борисов on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Effective1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
